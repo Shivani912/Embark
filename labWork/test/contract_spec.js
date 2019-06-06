f4d382ca78074e8fbae7868a8432499dbb97e073
Dima Kokarovtsev <dima.kokarovtsev@gmail.com>
2019-06-06 13:05:11 -0400

    Added negative test case for addTranscriptHash

--- labWork/test/contract_spec.js
+++ labWork/test/contract_spec.js
@@ -1,6 +1,5 @@
-
 const transcriptVerification = require('Embark/contracts/transcriptVerification');
-
+const _transcriptHash = "0x54e6289e14c7b0e7ad9acc2dfc4c1e3d027d0eef7f5c4c3fe7c292761d0e06a6"
 let accounts;
 
 // For documentation please see https://embark.status.im/docs/contracts_testing.html
@@ -27,8 +26,17 @@ contract("transcriptVerification", function () {
   })
 
   it("Owner adds transcript hash", async function () {
-    let _transcriptHash = "0x54e6289e14c7b0e7ad9acc2dfc4c1e3d027d0eef7f5c4c3fe7c292761d0e06a6"
     await transcriptVerification.methods.addTranscriptHash(accounts[2], _transcriptHash).send();
     assert(await transcriptVerification.methods.viewTranscript().call({from:accounts[2]}) === _transcriptHash)
+  })
+
+  it("Non-owner adds transcript hash", async function () {
+    try {
+      await transcriptVerification.methods.addTranscriptHash(
+        accounts[2], _transcriptHash).send({from:accounts[1]});
+      assert(false);
+    } catch (error) {
+      assert(error.message.includes("you are not the owner"))
+    }
   })
 })
