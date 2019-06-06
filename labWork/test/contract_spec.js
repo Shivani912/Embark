aaa9fe1eb4519ac2d33bce39478ce2bc534efcba
Dima Kokarovtsev <dima.kokarovtsev@gmail.com>
2019-06-06 12:54:04 -0400

    Added positive test case for addTranscriptHash

--- labWork/test/contract_spec.js
+++ labWork/test/contract_spec.js
@@ -12,7 +12,7 @@ config({
   //  ]
   //},
   contracts: {
-    "transcriptVerification": { }
+    "transcriptVerification": {}
   }
 }, (_err, web3_accounts) => {
   accounts = web3_accounts
@@ -24,5 +24,11 @@ contract("transcriptVerification", function () {
   it("Constructor runs successfully", async function () {
     let address = transcriptVerification.options.address;
     assert.ok(address);
+  })
+
+  it("Owner adds transcript hash", async function () {
+    let _transcriptHash = "0x54e6289e14c7b0e7ad9acc2dfc4c1e3d027d0eef7f5c4c3fe7c292761d0e06a6"
+    await transcriptVerification.methods.addTranscriptHash(accounts[2], _transcriptHash).send();
+    assert(await transcriptVerification.methods.viewTranscript().call({from:accounts[2]}) === _transcriptHash)
   })
 })
