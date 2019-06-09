af3ac182015ecf952d42228e7ee2ff264344c4e9
Shivani912 <46731446+Shivani912@users.noreply.github.com>
2019-06-09 17:57:02 -0400

    Removed unwanted lines

--- labWork/test/contract_spec.js
+++ labWork/test/contract_spec.js
@@ -3,14 +3,8 @@ const _transcriptHash = "0x54e6289e14c7b0e7ad9acc2dfc4c1e3d027d0eef7f5c4c3fe7c29
 let accounts;
 const _fakeTranscriptHash = "0x54e6289e14c7b0e7ad9acc2dfc4c1e3d027d0eef7f5c4c3fe7c292761d0e06a9"
 
-// For documentation please see https://embark.status.im/docs/contracts_testing.html
+
 config({
-  //deployment: {
-  //  accounts: [
-  //    // you can configure custom accounts with a custom balance
-  //    // see https://embark.status.im/docs/contracts_testing.html#Configuring-accounts
-  //  ]
-  //},
   contracts: {
     "transcriptVerification": {}
   }
